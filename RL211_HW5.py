a4fce2c56d5cf12e1d486e34dbeda0608458c04a
noamsgl <noamsi@post.bgu.ac.il>
2021-02-14 23:17:04 +0200

    Update Project

--- RL211_HW5.py
+++ RL211_HW5.py
@@ -3,22 +3,28 @@ from collections import deque
 import gym
 import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 from PIL import Image
 from tensorflow.keras import layers
 
+# Config GPU
+tf.disable_v2_behavior()
 
-def preprocess(S):
-    im = Image.fromarray(np.uint8(S))
-    im = im.convert('YCbCr')
-    Y, Cb, Cr = im.split()
-    Y = Y.resize(size=(84, 84))
-    Y = np.array(Y)
-    Y = Y[..., np.newaxis]
-    return Y
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(config=config)
 
 
 def modify_env(env, p=1):
+    def preprocess(S):
+        im = Image.fromarray(np.uint8(S))
+        im = im.convert('YCbCr')
+        Y, Cb, Cr = im.split()
+        Y = Y.resize(size=(84, 84))
+        Y = np.array(Y)
+        Y = Y[..., np.newaxis]
+        return Y
+
     def new_reset():
         S = env.orig_reset()
         return preprocess(S)
@@ -37,103 +43,21 @@ def modify_env(env, p=1):
     env.step = new_step
     return env
 
-
-def evaluate_Q_Network(q_learn, num_episodes=100):
-    values = []
-    for iter in range(num_episodes):
-        S = q_learn.env.reset()
-        value = 0
-        while True:
-            A = q_learn.get_action(epsilon=0)
-            S, R, done, info = q_learn.env.step(A)
-            value += R
-            if done:
-                break
-        values.append(value)
-    return np.mean(values)
-
-
 #
-# def get_action(S, nA, model, epsilon=0.0):
-#     if np.random.rand() < epsilon:
-#         return np.random.randint(nA)
-#     else:
-#         Q_values = model.predict(S[np.newaxis])
-#         return np.argmax(Q_values[0])
+# def evaluate_Q_Network(q_learn, num_episodes=100):
+#     values = []
+#     for iter in range(num_episodes):
+#         S = q_learn.env.reset()
+#         value = 0
+#         while True:
+#             A = q_learn.get_action(epsilon=0)
+#             S, R, done, info = q_learn.env.step(A)
+#             value += R
+#             if done:
+#                 break
+#         values.append(value)
+#     return np.mean(values)
 #
-#
-# def play_step(env, S, model, epsilon, buffer):
-#     action = get_action(S, env.action_space.n, model, epsilon)
-#     S_tag, R, done, info = env.step(action)
-#     buffer.append([S, action, R, S_tag])
-#     return S_tag, R, done, info
-#
-#
-# def get_experiences(buffer, batch_size):
-#     idxs = np.random.randint(len(buffer), size=batch_size)
-#     batch = [buffer[i] for i in idxs]
-#     states, actions, rewards, next_states, dones = [
-#         np.array([experience[field_index] for experience in batch])
-#         for field_index in range(5)
-#     ]
-#     return states, actions, rewards, next_states, dones
-#
-#
-# def training_step(batch_size, nA, gamma, model, loss_function, optimizer, buffer):
-#     experiences = get_experiences(buffer, batch_size)
-#     states, actions, rewards, next_states, dones = experiences
-#     next_Q_values = model.predict(next_states)
-#     max_next_Q_values = np.max(next_Q_values, axis=1)
-#     target_Q_values = (rewards + (1 - dones) * gamma * max_next_Q_values)
-#     mask = tf.one_hot(actions, nA)
-#     with tf.GradientTape() as tape:
-#         all_Q_values = model(states)
-#         Q_values = tf.reduce_sum(all_Q_values * mask, axis=1, keepdims=True)
-#         loss = tf.reduce_mean(loss_function(target_Q_values, Q_values))
-#         grads = tape.gradient(loss, model.trainable_variables)
-#         optimizer.apply_gradients(zip(grads, model.trainable_variables))
-#     return model
-#
-#
-# def Deep_Q_Learning(env, network, max_time_steps):
-#     epsilon = 0.5
-#     batch_size = 64
-#     nA = env.action_space.n
-#     gamma = 0.95
-#     if network == "DQN":
-#         model = tf.keras.models.Sequential([
-#             layers.Conv2D(filters=32, kernel_size=(8, 8), strides=4, activation="relu", input_shape=(84, 84, 1)),
-#             layers.Conv2D(filters=64, kernel_size=(4, 4), strides=2, activation="relu"),
-#             layers.Conv2D(filters=64, kernel_size=(3, 3), strides=1, activation="relu"),
-#             layers.Flatten(),
-#             layers.Dense(units=512, activation="relu"),
-#             layers.Dense(units=18)
-#         ])
-#     elif network == "DRQN":
-#         model = tf.keras.models.Sequential([
-#             layers.Conv2D(filters=32, kernel_size=(8, 8), strides=4, activation="relu", input_shape=(84, 84, 1)),
-#             layers.Conv2D(filters=64, kernel_size=(4, 4), strides=2, activation="relu"),
-#             layers.Conv2D(filters=64, kernel_size=(3, 3), strides=1, activation="relu"),
-#             layers.LSTM(units=512),
-#             layers.Dense(units=18)
-#         ])
-#     else:
-#         raise ValueError
-#     buffer = deque(maxlen=2000)
-#     loss_function = keras.losses.mean_squared_error
-#     optimizer = keras.optimizers.Adam(lr=1e-3)
-#
-#     S = env.reset()
-#     for t in range(max_time_steps):
-#         env.render()
-#         epsilon = 0.99 * epsilon
-#         S, R, done, info = play_step(env, S, model, epsilon, buffer)
-#         if done:
-#             S = env.reset()
-#         if t > 10000:
-#             model = training_step(batch_size, nA, gamma, model, loss_function, optimizer, buffer)
-#
-#     return model
 
 class Q_Learn:
     def __init__(self, env, network, max_time_steps, batch_size=64, gamma=0.95, epsilon=0.5, eval_X=200):
@@ -164,23 +88,26 @@ class Q_Learn:
                 layers.Conv2D(filters=32, kernel_size=(8, 8), strides=4, activation="relu", input_shape=(84, 84, 1)),
                 layers.Conv2D(filters=64, kernel_size=(4, 4), strides=2, activation="relu"),
                 layers.Conv2D(filters=64, kernel_size=(3, 3), strides=1, activation="relu"),
+                layers.Reshape((1, 3136)),
                 layers.LSTM(units=512),
                 layers.Dense(units=18)
             ])
         else:
             raise ValueError
 
-    def evaluate(self, num_episodes=100):
+    def evaluate(self, num_episodes=5, max_episode_len=200):
         values = []
         for iter in range(num_episodes):
             self.env.reset()
             value = 0
-            while True:
+            for i in range(max_episode_len):
+                self.env.render()
                 A = self.get_action(epsilon=0)
                 S, R, done, info = self.env.step(A)
                 value += R
                 if done:
                     break
+            print("Evaluating: iter {} of {}, Value: {}".format(iter, num_episodes, value))
             values.append(value)
         return np.mean(values)
 
@@ -200,7 +127,7 @@ class Q_Learn:
                 self.S = self.env.reset()
             if t > len(self.buffer):
                 self.training_step()
-        return model
+        return self.model
 
     def play_step(self, action):
         S_tag, R, done, info = self.env.step(action)
@@ -239,16 +166,15 @@ class Q_Learn:
 
 
 if __name__ == '__main__':
-    num_time_steps = 200000
-
+    max_time_steps = 300
+    print(tf.config.get_visible_devices('GPU'))
     # graph number 1
     for network in ["DQN", "DRQN"]:
         env = modify_env(gym.make('Frostbite-v0'), p=1)
-        q_learn = Q_Learn(env, network, num_time_steps)
+        q_learn = Q_Learn(env, network, max_time_steps)
         model = q_learn.run(eval=True)
         plt.plot(q_learn.X, q_learn.Y, label=network)
         env.close()
-
     plt.show()
 
     #     graph number 2
@@ -257,7 +183,7 @@ if __name__ == '__main__':
         X = np.linspace(1.0, 0.0, 11, True)
         for p in X:
             env = modify_env(gym.make('Frostbite-v0'), p=p)
-            q_learn = Q_Learn(env, network, num_time_steps)
+            q_learn = Q_Learn(env, network, max_time_steps)
             model = q_learn.run()
             scores[p] = q_learn.evaluate()
             env.close()
